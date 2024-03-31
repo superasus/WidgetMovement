91e701f8898c0caa1925f7fb6c4e928f982cb72e
Гутин Дмитрий <gutindima17@gmail.com>
2024-03-31 14:47:18 +0500

    WidgetMovement Gutin task implementation

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include <QTimer>
 #include <QPushButton>
+#include <QRandomGenerator>
 
 #include "mainwindow.h"
 
@@ -7,10 +8,48 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     resize(300, 600);
-/// Your code here...
-
+    m_timer = new QTimer();
+    m_timer->setInterval(QRandomGenerator::global()->bounded(100, 1000));
+    connect(m_timer, &QTimer::timeout,
+            [wnd = this]{
+                auto pb = new QPushButton("*", wnd);
+                pb->setStyleSheet("QPushButton{"
+                                  "background-color: aliceblue;"
+                                  "border: 1px solid gray;"
+                                  "border-radius: 3px;} "
+                                  "QPushButton:hover {"
+                                  "background-color: powderblue;} ");
+                int buttonWidth = 20;
+                pb->setGeometry(QRandomGenerator::global()->bounded(wnd->width() - buttonWidth),
+                    QRandomGenerator::global()->bounded(100),
+                    buttonWidth, buttonWidth);
+                pb->show();
+                auto moveTimer = new QTimer(pb);
+                moveTimer->setInterval(QRandomGenerator::global()->bounded(70, 250));
+                connect(moveTimer, &QTimer::timeout,
+                        [pb, wnd]{
+                            int posShift = 3;
+                            if(pb->underMouse()) {
+                                posShift *= 2;
+                            }
+                            pb->move(pb->pos() + QPoint(0, posShift));
+                            if(pb->pos().y() > wnd->height() - pb->height()) {
+                                wnd->setStyleSheet("background-color: red;");
+                                wnd->setWindowTitle("YOU LOSE!");
+                                pb->deleteLater();
+                            }
+                        });
+                connect(pb, &QPushButton::clicked,
+                        [pb]{
+                            pb->deleteLater();
+                        });
+                moveTimer->start();
+                wnd->m_timer->setInterval(QRandomGenerator::global()->bounded(100, 1000));
+            });
+    m_timer->start();
 }
 
 MainWindow::~MainWindow()
 {
+    delete m_timer;
 }
