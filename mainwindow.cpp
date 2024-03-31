2844fcd1fcfaf24befec5c282473f14447a53280
Гутин Дмитрий <gutindima17@gmail.com>
2024-03-31 14:33:57 +0500

    Init

---
+++ mainwindow.cpp
@@ -0,0 +1,16 @@
+#include <QTimer>
+#include <QPushButton>
+
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    resize(300, 600);
+/// Your code here...
+
+}
+
+MainWindow::~MainWindow()
+{
+}
