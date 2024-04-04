e33653ff8dddbb14ef2f692fd2ea1b46c363ce09
Гутин Дмитрий <gutindima17@gmail.com>
2024-04-04 20:23:26 +0500

    Исправление замечаний

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,48 +8,41 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     resize(300, 600);
-    m_timer = new QTimer();
+    m_timer = new QTimer(this);
     m_timer->setInterval(QRandomGenerator::global()->bounded(100, 1000));
     connect(m_timer, &QTimer::timeout,
-            [wnd = this]{
-                auto pb = new QPushButton("*", wnd);
+            [this]{
+                auto pb = new QPushButton("*", this);
                 pb->setStyleSheet("QPushButton{"
                                   "background-color: aliceblue;"
                                   "border: 1px solid gray;"
                                   "border-radius: 3px;} "
                                   "QPushButton:hover {"
                                   "background-color: powderblue;} ");
-                int buttonWidth = 20;
-                pb->setGeometry(QRandomGenerator::global()->bounded(wnd->width() - buttonWidth),
+                constexpr int buttonWidth = 20;
+                pb->setGeometry(QRandomGenerator::global()->bounded(width() - buttonWidth),
                     QRandomGenerator::global()->bounded(100),
                     buttonWidth, buttonWidth);
                 pb->show();
                 auto moveTimer = new QTimer(pb);
                 moveTimer->setInterval(QRandomGenerator::global()->bounded(70, 250));
                 connect(moveTimer, &QTimer::timeout,
-                        [pb, wnd]{
-                            int posShift = 3;
-                            if(pb->underMouse()) {
-                                posShift *= 2;
-                            }
+                        [pb, this]{
+                            const int posShift = pb->underMouse() ? 6 : 3;
                             pb->move(pb->pos() + QPoint(0, posShift));
-                            if(pb->pos().y() > wnd->height() - pb->height()) {
-                                wnd->setStyleSheet("background-color: red;");
-                                wnd->setWindowTitle("YOU LOSE!");
+                            if(pb->pos().y() > height() - pb->height()) {
+                                setStyleSheet("background-color: red;");
+                                setWindowTitle("YOU LOSE!");
                                 pb->deleteLater();
                             }
                         });
-                connect(pb, &QPushButton::clicked,
-                        [pb]{
-                            pb->deleteLater();
-                        });
+                connect(pb, &QPushButton::clicked, pb, &QPushButton::deleteLater);
                 moveTimer->start();
-                wnd->m_timer->setInterval(QRandomGenerator::global()->bounded(100, 1000));
+                m_timer->setInterval(QRandomGenerator::global()->bounded(100, 1000));
             });
     m_timer->start();
 }
 
 MainWindow::~MainWindow()
 {
-    delete m_timer;
 }
